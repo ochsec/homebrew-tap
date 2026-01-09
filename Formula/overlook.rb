0cee11063fce6fae0fbbfecf1fe71c64987cee13
Christopher Ochsenreither <cochsenreither@protonmail.com>
2026-01-08 21:14:44 -0700

    updated overlook and deleted overlook-tui

--- Formula/overlook.rb
+++ Formula/overlook.rb
@@ -1,28 +1,28 @@
 class Overlook < Formula
   desc "Terminal UI chat application for LLM providers"
   homepage "https://github.com/ochsec/overlook-tui"
-  version "0.3.2"
+  version "0.3.3"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/ochsec/overlook-tui/releases/download/v.#{version}/overlook-v.#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "252c3e48d08a1e32a6dbffbde954d85e661e8063d87f9a9603e8a1bb6f5eab19"
+      url "https://github.com/ochsec/overlook-tui/releases/download/v#{version}/overlook-v#{version}-aarch64-apple-darwin.tar.gz"
+      sha256 "bf3e8434f7585dd5a97642ec636037ce3a1aac1fd826445d9afe7cf1d1120552"
     end
     on_intel do
-      url "https://github.com/ochsec/overlook-tui/releases/download/v.#{version}/overlook-v.#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "448d08cba317a9cce06939f0384609f9abdc43909513f8dab909df5761fc46bf"
+      url "https://github.com/ochsec/overlook-tui/releases/download/v#{version}/overlook-v#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "15247ba53a8a20eb7c1a6857949329aac38243d2987a7a0cc6b1de616399712c"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/ochsec/overlook-tui/releases/download/v.#{version}/overlook-v.#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "b6e8bf6fb99faafd85018a7fb2bf12dc1d5a5dc60faaadeb962ba7e8e9bc23c2"
+      url "https://github.com/ochsec/overlook-tui/releases/download/v#{version}/overlook-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "d24df945c74acef9dce16a328bfc7f739a6483037a7aa9912a09ec5e53723c8e"
     end
     on_intel do
-      url "https://github.com/ochsec/overlook-tui/releases/download/v.#{version}/overlook-v.#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "21d7f76df290cd450227cfdc540d617780a62a4aa4ad03035b858b9b488b3095"
+      url "https://github.com/ochsec/overlook-tui/releases/download/v#{version}/overlook-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "f874a8104c0bc9572476f085e1f222d363f1b448915cf6d2eefcc123200e0a86"
     end
   end
 
