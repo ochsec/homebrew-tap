325a9dc10720bc10c103be20b509dcbdee816e41
Christopher Ochsenreither <cochsenreither@protonmail.com>
2026-01-09 23:35:51 -0700

    updated overlook to v0.3.4

--- Formula/overlook.rb
+++ Formula/overlook.rb
@@ -1,28 +1,28 @@
 class Overlook < Formula
   desc "Terminal UI chat application for LLM providers"
   homepage "https://github.com/ochsec/overlook-tui"
-  version "0.3.3"
+  version "0.3.4"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/ochsec/overlook-tui/releases/download/v#{version}/overlook-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "bf3e8434f7585dd5a97642ec636037ce3a1aac1fd826445d9afe7cf1d1120552"
+      sha256 "9c57e9f1f6cb533352c4bec60cd5554dc4c31b6d1389f2250e17c10472e7285b"
     end
     on_intel do
       url "https://github.com/ochsec/overlook-tui/releases/download/v#{version}/overlook-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "15247ba53a8a20eb7c1a6857949329aac38243d2987a7a0cc6b1de616399712c"
+      sha256 "80d3531bd6d6a01b004265b4b0835852ac248094ea6158f601b634010a3a50c8"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/ochsec/overlook-tui/releases/download/v#{version}/overlook-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "d24df945c74acef9dce16a328bfc7f739a6483037a7aa9912a09ec5e53723c8e"
+      sha256 "269f8d1e1b42d3aeea0114b0c779b1d8d18dd16be80cb11d7c77d5a585113b55"
     end
     on_intel do
       url "https://github.com/ochsec/overlook-tui/releases/download/v#{version}/overlook-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "f874a8104c0bc9572476f085e1f222d363f1b448915cf6d2eefcc123200e0a86"
+      sha256 "6cc738ce607de4f5fa62f59d57c224c670e38585833ffbc3db167fecec5a02d9"
     end
   end
 
